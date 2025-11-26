183e3b33d859ebfa4e184a4745a24c811c3118d3
arthurwieb <arthur.wieb@gmail.com>
2025-11-26 00:18:17 -0300

    deploy

--- vite.spa.config.ts
+++ vite.spa.config.ts
@@ -1,22 +1,18 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
-
-const config = defineConfig({
+export default defineConfig({
   plugins: [
-    devtools(),
-    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart(),
     viteReact(),
   ],
+  build: {
+    outDir: 'dist/client',
+    emptyOutDir: true,
+  }
 })
-
-export default config
